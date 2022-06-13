acf33178415d560ed24dcba5fa890f1e9c120b9e
Matthew McMorries <85896879+TomHawk123@users.noreply.github.com>
2022-06-13 15:52:14 -0500

    ZASUserView and PostView complete

---
+++ app_api/views/posts.py
@@ -0,0 +1,71 @@
+"""View module for handling requests about zas_users"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from app_api.models.zas_user import ZASUser
+from app_api.models.post import Post
+from app_api.views.zas_users import ZASUserSerializer
+
+
+class PostView(ViewSet):
+    """View for handling Post requests"""
+
+    def retrieve(self, request, pk):
+        """PUT method handler"""
+        try:
+            post = Post.objects.get(pk=pk)
+            serializer = PostSerializer(post)
+            return Response(serializer.data)
+        except Post.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+    def list(self, request):
+        """GET list of all posts"""
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        """POST Post handler"""
+        author = ZASUser.objects.get(user=request.auth.user)
+        serializer = CreatePostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=author)
+        post = Post.objects.get(pk=serializer.data['id'])
+        post.tags.add(*request.data['tags'])
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+      
+    def destroy(self, request, pk):
+        """DELETE request handler for Post"""
+        post = Post.objects.get(pk=pk)
+        post.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+
+class PostSerializer(serializers.ModelSerializer):
+    """JSON serializer for Posts
+    The serializer class determines how the Python data should
+    be serialized to be sent back to the client.
+    """
+    class Meta:
+        model = Post
+        fields = (
+            'id',
+            'author',
+            'title',
+            'created_on',
+            'body',
+            'tags'
+        )
+
+
+class CreatePostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = (
+            'id',
+            'title',
+            'created_on',
+            'body',
+            'tags')
