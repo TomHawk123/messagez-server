24dfb23519057fa47198cfe4f3124ec316de829a
Matthew McMorries <85896879+TomHawk123@users.noreply.github.com>
2022-06-13 13:29:09 -0500

    fixtures loaded and models corrected.

--- app_api/models/message.py
+++ app_api/models/message.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 class Message(models.Model):
-    post = models.ForeignKey("Post", on_delete=models.CASCADE)
-    sender = models.ForeignKey("ZASUser", on_delete=models.CASCADE)
+    sender = models.ForeignKey("ZASUser", on_delete=models.CASCADE, related_name='author')
     content = models.CharField(max_length=1000)
     recipient = models.ForeignKey("ZASUser", on_delete=models.CASCADE)
     created_on = models.DateField(auto_now=True)
--- app_api/models/zas_user.py
+++ app_api/models/zas_user.py
@@ -3,7 +3,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class ZASUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     bio = models.CharField(max_length=1000)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_on = models.DateField(auto_now=True)
